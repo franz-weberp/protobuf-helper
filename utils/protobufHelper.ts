f3c856bd345007f30d67ec383c6cdcb8d71550b1
Franz Weber Pimentel <fweber.pimentel@gmail.com>
2024-06-18 07:02:52 -0300

    setup compile and export Message from protobuf

--- utils/protobufHelper.ts
+++ utils/protobufHelper.ts
@@ -1,12 +1,20 @@
 import protobuf from 'protobufjs'
 import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
+import { dirname, resolve } from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-const protoFilePath = join(__dirname, 'protobuf/schema.proto')
+const protoFilePath = resolve(__dirname, './schema.proto')
 
-const root = protobuf.loadSync(protoFilePath)
-const Message = root.lookupType('Message')
 
-export { Message }
+function exportMessage() {
+  try {
+    const root = protobuf.loadSync(protoFilePath)
+    const Message = root.lookupType('Message')
+    return Message
+  } catch (error) {
+    console.error('Error loading schema.proto:', error)
+  }
+}
+
+export default exportMessage()
